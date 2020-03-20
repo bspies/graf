941f0a2906aa5be635c1f99ee920ff48f895da92
Brennan Spies <brennanspies@gmail.com>
2020-03-20 08:08:17 -0700

    Minor changes to graph structures: add constructor and doc comments

--- src/main/java/org/graf/directed/GenericDigraph.java
+++ src/main/java/org/graf/directed/GenericDigraph.java
@@ -10,20 +10,38 @@ import java.util.stream.Collectors;
 public class GenericDigraph<V, E extends Edge<V>> extends AbstractGraph<V,E> implements DirectedGraph<V,E> {
 
     /**
-     * Creates the graph with the initial nodes and edges.
+     * Creates the directed graph with the initial nodes and edges.
      */
     public GenericDigraph() {
         super(new HashSet<>(), new HashSet<>());
     }
 
+    /**
+     * Creates the directed graph with the initial nodes and edges.
+     * @param initialNodes The initial nodes
+     * @param initialEdges The initial edges
+     */
+    public GenericDigraph(Set<V> initialNodes, Set<E> initialEdges) {
+        super(new HashSet<>(initialNodes), new HashSet<>(initialEdges));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override public Set<E> getOutgoingEdges(V node) {
         return getEdges().stream().filter(e -> node.equals(e.getSource())).collect(Collectors.toSet());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override public Set<E> getIncomingEdges(V node) {
         return getEdges().stream().filter(e -> node.equals(e.getTarget())).collect(Collectors.toSet());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override public boolean isAcyclic() {
         return false;
     }
--- src/main/java/org/graf/undirected/GenericGraph.java
+++ src/main/java/org/graf/undirected/GenericGraph.java
@@ -7,15 +7,32 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * An implementation of an undirected graph.
+ * @param <V> The node value type
+ * @param <E> The edge type
+ */
 public class GenericGraph<V, E extends Edge<V>> extends AbstractGraph<V,E> implements UndirectedGraph<V,E> {
 
     /**
-     * Creates the graph.
+     * Creates the undirected graph.
      */
     public GenericGraph() {
         super(new HashSet<>(), new HashSet<>());
     }
 
+    /**
+     * Creates a the undirected graph with initial nodes and edges.
+     * @param initialNodes The initial nodes
+     * @param initialEdges The initial edges
+     */
+    public GenericGraph(Set<V> initialNodes, Set<E> initialEdges) {
+        super(new HashSet<>(initialNodes), new HashSet<>(initialEdges));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override public Set<E> getEdgesOf(V node) {
         return getEdges().stream()
                 .filter(e -> node.equals(e.getSource()) || node.equals(e.getTarget()))
